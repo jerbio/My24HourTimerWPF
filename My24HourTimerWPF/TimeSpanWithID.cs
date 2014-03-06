a6c0be2d881f4d1566fcf79729b4848a49056724
jerbio <jeromebiotidara@gmail.com>
2014-03-06 16:41:39 -0700

    Implemented spreadOut ALgorithm & Procrastinate
    ***Current Updata***
    Current implementation uses EvaluateTotalTimeLineAndAssignValidTimeSpots to always reassign schedule irrespective of how much space is available. It no longer needs the artificial schedule breaker.
    
    Implemented Procrastinate
    
    Spreadout also tries to assign events based on the average occupancy of the schedule.
    
    ****BUGS****
    -there is a dictionary key bug that needs to be fixed. Check your debug folder for keyerror.xml.
    -Even though spread out evens out the occupancy, it does not try to spread out the schedule.
    -Also we need another call to stitchunrestricted to ensure further evening out of schedules
    
    -Using a procrastinate button while working on an event needs to accomodate the curent event being worked on
    
    - need real life location data in implementation
    - need to ensure that selected events are prioritized based on closeness to deadline.
    - implement enforcement of diversity in events
    - Disable stitch restricted for last free time line, or ensure that it  only stitches event that cannot exist outside the deadline

---
+++ My24HourTimerWPF/TimeSpanWithID.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace My24HourTimerWPF
+{
+    public class TimeSpanWithEventID
+    {
+        public TimeSpan timeSpan;
+        public EventID TimeSpanID;
+        public TimeSpanWithEventID(TimeSpan TimeSpanEntry, EventID ID)
+        {
+            timeSpan = TimeSpanEntry;
+            TimeSpanID = ID;
+        }
+        public TimeSpanWithEventID(long ticks,EventID ID)
+        {
+            timeSpan=new TimeSpan(ticks);
+            TimeSpanID = ID;
+        }
+        public TimeSpanWithEventID(int hours, int minutes, int seconds,EventID ID)
+        {
+            timeSpan = new TimeSpan(hours,  minutes,  seconds);
+            TimeSpanID = new EventID(ID.ToString());
+        }
+        
+        public TimeSpanWithEventID(int days, int hours, int minutes, int seconds, EventID ID)
+        {
+            timeSpan = new TimeSpan(days,hours, minutes, seconds);
+            TimeSpanID = new EventID(ID.ToString());
+        }
+        
+        public TimeSpanWithEventID(int days, int hours, int minutes, int seconds, int milliseconds, EventID ID)
+        {
+            timeSpan = new TimeSpan(days, hours, minutes, seconds);
+            TimeSpanID = new EventID(ID.ToString());
+        }
+        
+    }
+
+
+    public class TimeSpanWithStringID
+    {
+        public TimeSpan timeSpan;
+        public string ID;
+        
+        public TimeSpanWithStringID(TimeSpan Arg1, string Arg2)
+        {
+            timeSpan = Arg1;
+            ID = Arg2;
+        }
+
+        public override string ToString()
+        {
+            return timeSpan.ToString();
+        }
+    }
+  
+}

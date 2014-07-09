894af142af2c39f73b89e63a697b877289d41825
jerbio <jeromebiotidara@gmail.com>
2014-07-08 22:21:38 -0600

    Just commiting another file

--- My24HourTimerWPF/UserAccount.cs
+++ My24HourTimerWPF/UserAccount.cs
@@ -1,4 +1,4 @@
-﻿#define readfromBeforeInsertionFixingStiticRestricted
+﻿//#define readfromBeforeInsertionFixingStiticRestricted
 using System;
 using System.Collections.Generic;
 using System.Linq;

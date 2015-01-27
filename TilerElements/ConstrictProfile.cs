aeb40a5174ec7fac5ff1c1bc0f2e3ebfc60ae2d9
Jerome Biotidara <jeromebiotidara@gmail.com>
2015-01-26 19:44:32 -0700

    Implemented conflict profile

--- TilerElements/ConstrictProfile.cs
+++ TilerElements/ConstrictProfile.cs
@@ -7,12 +7,59 @@ namespace TilerElements
 {
     public class ConstrictProfile
     {
-        DateTime Start;
-        DateTime End;
-        public ConstrictProfile(DateTime StartTime,DateTime EndTime)
+        DateTimeOffset StartTime;
+        TimeSpan SpanOfConstriction;
+        static TimeSpan ZeroTimeSpan = new TimeSpan(0);
+        bool isSet;
+
+
+        public ConstrictProfile()
         {
-            Start = StartTime;
-            End=EndTime;
+            StartTime = new DateTimeOffset();
+            SpanOfConstriction = new TimeSpan();
+            isSet = false;
+        }
+
+        public ConstrictProfile(DateTimeOffset Start, DateTimeOffset End)
+        {
+            Start =  new DateTimeOffset(1,1,1,Start.Hour,Start.Minute,Start.Second,new TimeSpan());
+            End = new DateTimeOffset(1, 1, 1, End.Hour, End.Minute, End.Second, new TimeSpan());
+
+            TimeSpan Span = End - Start;
+            if ((Span <= new TimeSpan(1, 0, 0, 0)) && (Span >= ZeroTimeSpan))
+            {
+                StartTime = Start;
+                SpanOfConstriction =Span;
+                isSet = true;
+                if ((new TimeSpan(0)).Ticks == 0)
+                {
+                    isSet = false;
+                }
+            }
+            else
+            {
+                throw new Exception("SpanOfConstriction is invalid span is less than zerospan or greater than twenty four hours");
+            }
+
+            
+        }
+
+        public ConstrictProfile(DateTimeOffset Start, TimeSpan Span)
+        {
+            if ((Span <= new TimeSpan(1, 0, 0, 0)) && (Span >= ZeroTimeSpan))
+            {
+                StartTime = Start;
+                SpanOfConstriction = Span;
+                isSet = true;
+                if ((new TimeSpan(0)).Ticks == 0)
+                {
+                    isSet = false;
+                }
+            }
+            else
+            {
+                throw new Exception("SpanOfConstriction is invalid span is less than zerospan or greater than twenty four hours");
+            }
         }
     }
 }

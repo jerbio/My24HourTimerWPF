0a05c6ea8dbcd49287424a3a2452e39fcb5272a1
Jerome Biotidara <jeromebiotidara@gmail.com>
2015-06-08 20:40:47 -0600

    Implpemented rudimentary path optimization. It has not being tested at all. very unstable code. Added OptimizedPath class. Represents the static class that generates an optimiazed  path for a given day. It classifies the events in t day section and makes a best effort to create best path.

---
+++ TilerElements/TimeOfDayPreferrence.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TilerElements
+{
+    public class TimeOfDayPreferrence
+    {
+        public enum DaySection {Sleep,Morning,Afternoon,Evening, None,Disabled}
+        Stack<Tuple<int, DaySection, bool>> PreferenceOrder = new Stack<Tuple<int, DaySection, bool>>(new[] { new Tuple<int, DaySection, bool>(4, DaySection.Morning, false), new Tuple<int, DaySection, bool>(3, DaySection.Evening, false), new Tuple<int, DaySection, bool>(2, DaySection.Afternoon, false), new Tuple<int, DaySection, bool>(1, DaySection.Evening, false), new Tuple<int, DaySection, bool>(0, DaySection.None, false) });
+        //TilerEvent ControlEvent;
+        public TimeOfDayPreferrence()
+        {
+        }
+
+        internal void InitializeGrouping(TilerEvent ControlEvent)
+        {
+            return;
+        }
+        public DaySection getCurrentDayPreference()
+        {
+            if(PreferenceOrder.Count>0)
+            {
+                return PreferenceOrder.Peek().Item2;
+            }
+            else
+            {
+                return DaySection.Disabled;
+            }
+        }
+
+        public void rejectCurrentPreference()
+        {
+            if(PreferenceOrder.Count>0)
+            {
+                PreferenceOrder.Pop();
+            }
+        }
+    }
+}

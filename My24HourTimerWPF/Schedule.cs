c5db5cb6d8bd1c9010759cf04728266aa9acf36c
Jerome Biotidara <jeromebiotidara@gmail.com>
2016-02-01 21:04:25 -0500

    Added better optimization. Uses multidimensional and fibonnacci

--- My24HourTimerWPF/Schedule.cs
+++ My24HourTimerWPF/Schedule.cs
@@ -11381,6 +11381,8 @@ namespace My24HourTimerWPF
         static TimeLine ScheduleTimeline = new TimeLine();
 
 
+        //public XmlElement CreateEventScheduleNode(CalendarEvent MyEvent, XmlDocument xmldoc)
+
         public void LogStatus(CalendarEvent triggerEvent, string Trigger)//writes to an XML Log file. Takes calendar event as an argument
         {
 #if EnableClashLog

e3d45479a8635617c98cdb4a3fbc01f20804600c
Jerome Biotidara <jeromebiotidara@gmail.com>
2016-03-11 01:36:19 -0500

    Added the useractivity class to allow for tracking types of user interaction

---
+++ TilerElements/UserActivity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TilerElements
+{
+    public class UserActivity
+    {
+        ReferenceNow TriggerTimeForEvent;
+        public enum ActivityType {ProcrastinateSingle, ProcrastinateAll, ProcrastinateCalendarEvent, AdjustDeadline, CompleteSingle, CompleteMultiple, CompleteCalendarEvent, DeleteSingle, DeleteMultiple, DeleteCalendarEvent, NewEventCreation, None};
+        ActivityType Type;
+        public UserActivity(ReferenceNow triggerTime, ActivityType type)
+        {
+            TriggerTimeForEvent = triggerTime;
+            Type = type;
+        }
+    }
+
+
+}

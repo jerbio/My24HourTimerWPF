48513e8a8ba5aa0c0e527a88dcb4790489e694bc
Jerome Biotidara <jeromebiotidara@gmail.com>
2016-03-11 14:27:38 -0500

    Added bigdata modifications for tiler machine learning

--- TilerElements/UserActivity.cs
+++ TilerElements/UserActivity.cs
@@ -8,13 +8,35 @@ namespace TilerElements
 {
     public class UserActivity
     {
-        ReferenceNow TriggerTimeForEvent;
-        public enum ActivityType {ProcrastinateSingle, ProcrastinateAll, ProcrastinateCalendarEvent, AdjustDeadline, CompleteSingle, CompleteMultiple, CompleteCalendarEvent, DeleteSingle, DeleteMultiple, DeleteCalendarEvent, NewEventCreation, None};
-        ActivityType Type;
-        public UserActivity(ReferenceNow triggerTime, ActivityType type)
+        protected DateTimeOffset TriggerTimeForEvent { get; set; }
+        public enum ActivityType {Creation, Undo, ThirdPartyUpdate, SetAsNowSingle, SetAsNowCalendarEvent, ProcrastinateSingle, ProcrastinateAll, ProcrastinateCalendarEvent, InternalUpdate, InternalUpdateCalendarEvent, CompleteSingle, CompleteMultiple, CompleteCalendarEvent, DeleteSingle, DeleteMultiple, DeleteCalendarEvent, NewEventCreation, None};
+        protected ActivityType Type { get; set; }
+        protected List<string> updatedIds = new List<string>();
+        public UserActivity(ReferenceNow triggerTime, ActivityType type, IEnumerable<String> ids = null): this(triggerTime.constNow, type, ids)
+        {
+            
+        }
+
+        public UserActivity(DateTimeOffset triggerTime, ActivityType type, IEnumerable<String> ids = null)
         {
             TriggerTimeForEvent = triggerTime;
             Type = type;
+            if (ids != null)
+            {
+                updatedIds.AddRange(ids);
+            }
+        }
+
+        public DateTimeOffset ActivityTriggerTime // format: 2011-11-11T15:05:46.473340ids6+01:00
+        {
+            get { return TriggerTimeForEvent;
+            }
+        }
+
+
+        public ActivityType TriggerType// format: 2011-11-11T15:05:46.4733406+01:00
+        {
+            get { return Type; }
         }
     }
 

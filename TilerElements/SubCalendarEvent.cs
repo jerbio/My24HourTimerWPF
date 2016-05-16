4c431a258ce36dd694ef9f262551bb1650be04c1
Jerome Biotidara <jeromebiotidara@gmail.com>
2016-05-16 00:48:19 -0400

    added initial section for why to tiler. Able to write to db. Need to create front end UI elements

--- TilerElements/SubCalendarEvent.cs
+++ TilerElements/SubCalendarEvent.cs
@@ -27,7 +27,6 @@ namespace TilerElements
         protected DateTimeOffset _LastReasonStartTimeChanged;
         protected Dictionary<TimeSpan, List<Reason>> ReasonsForCurrentPosition = new Dictionary<TimeSpan, List<Reason>>();
 
-
         #region Classs Constructor
         public SubCalendarEvent()
         { }
@@ -1110,7 +1109,6 @@ namespace TilerElements
                 return ReasonsForCurrentPosition;
             }
         }
-
         #endregion
 
     }

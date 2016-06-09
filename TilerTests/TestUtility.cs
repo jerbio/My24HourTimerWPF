10e3a0f15855493754de18f9d56655a3e80db452
Jerome Biotidara <jeromebiotidara@gmail.com>
2016-06-09 01:05:19 -0400

    Added the initial componenet for verifying different type of tests for tiler

---
+++ TilerTests/TestUtility.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TilerTests
+{
+    public static class TestUtility
+    {
+        const int _MonthLimit = 3;
+        static readonly DateTimeOffset _Start = DateTimeOffset.UtcNow.AddMonths(-MonthLimit);
+        static readonly Random _Rand = new Random((int)DateTimeOffset.Now.Ticks);
+        static readonly string _UserName = "TestUserTiler";
+        static readonly string _Password = "T35tU53r#";
+
+
+        public static int MonthLimit
+        {
+            get
+            {
+                return _MonthLimit;
+            }
+        }
+
+        public static DateTimeOffset Start
+        {
+            get
+            {
+                return _Start;
+            }
+        }
+        public static Random Rand
+        {
+            get { return _Rand; }
+        }
+
+        public static string UserName
+        {
+            get {
+                return _UserName;
+            }
+        }
+        public static string Password
+        {
+            get
+            {
+                return _Password;
+            }
+        }
+    }
+}
+

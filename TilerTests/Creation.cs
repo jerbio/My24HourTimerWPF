ae0039ad01044414ce61a1c7021e9258c49c0f68
Jerome Biotidara <jeromebiotidara@gmail.com>
2016-06-09 01:05:19 -0400

    Added the initial componenet for verifying different type of tests for tiler

--- TilerTests/Creation.cs
+++ TilerTests/Creation.cs
@@ -10,17 +10,28 @@ namespace TilerTests
     public class Creation
     {
         Schedule Schedule;
+        DateTimeOffset StartOfTime = new DateTimeOffset();
         CalendarEvent CalendarEvent1;
         CalendarEvent CalendarEvent2;
         CalendarEvent CalendarEvent3;
-        string UserName = "TestUserTiler";
-        string Password = "T35tU53r#";
+        
+
+        CalendarEvent generateCalendarEvent(TimeSpan duration, Repetition repetition, int splitCount = 1, bool rigidFlags = false,  DateTimeOffset Start = new DateTimeOffset() )
+        {
+            if(Start == StartOfTime)
+            {
+                Start = TestUtility.Start;
+            }
+            DateTimeOffset End = Start.AddMonths(TestUtility.Rand.Next() % TestUtility.MonthLimit);
+            CalendarEvent RetValue = new CalendarEvent("TestCalendarEvent" + Guid.NewGuid().ToString(), duration,Start, End, new TimeSpan(), new TimeSpan(),rigidFlags, repetition,splitCount,)
+        }
+
         [TestMethod]
         async public void TestMethod1()
         {
-            TilerFront.Models.LoginViewModel myLogin = new TilerFront.Models.LoginViewModel() { Username = UserName, Password = Password, RememberMe = true };
+            TilerFront.Models.LoginViewModel myLogin = new TilerFront.Models.LoginViewModel() { Username = TestUtility.UserName, Password = TestUtility.Password, RememberMe = true };
 
-            TilerFront.Models.AuthorizedUser AuthorizeUser = new TilerFront.Models.AuthorizedUser() { UserID = "065febec-d1fe-4c8b-bd32-548613d4479f", UserName = UserName };
+            TilerFront.Models.AuthorizedUser AuthorizeUser = new TilerFront.Models.AuthorizedUser() { UserID = "065febec-d1fe-4c8b-bd32-548613d4479f", UserName = TestUtility.UserName };
 
             UserAccount currentUser = await AuthorizeUser.getUserAccountDebug();// new UserAccountDebug("18");
             await currentUser.Login();

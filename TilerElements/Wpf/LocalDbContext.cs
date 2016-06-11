1ef5b0946b357a27a6a9f36c62febb22d4521f36
Jerome Biotidara <jeromebiotidara@gmail.com>
2016-06-11 07:59:05 -0400

    Removed onmodelcreation implementation because of invalid table initializations

--- TilerElements/Wpf/LocalDbContext.cs
+++ TilerElements/Wpf/LocalDbContext.cs
@@ -33,13 +33,5 @@ namespace TilerElements.Wpf
         {
             return new LocalDbContext();
         }
-
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<IdentityUserLogin>().HasKey<string>(l => l.UserId);
-            modelBuilder.Entity<IdentityRole>().HasKey<string>(r => r.Id);
-            modelBuilder.Entity<IdentityUserRole>().HasKey(r => new { r.RoleId, r.UserId });
-        }
     }
 }

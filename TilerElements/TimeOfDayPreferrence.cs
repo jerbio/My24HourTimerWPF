2f4bad6b55091e3d6cca09351fb402b8fdc28806
Jerome Biotidara <jeromebiotidara@gmail.com>
2016-08-12 22:13:09 -0400

    FIxed bug where unnecessary conflicts were created because of the pathstitched and acknowledged events were getting implemented

--- TilerElements/TimeOfDayPreferrence.cs
+++ TilerElements/TimeOfDayPreferrence.cs
@@ -9,20 +9,62 @@ namespace TilerElements
     public class TimeOfDayPreferrence
     {
         public enum DaySection {Sleep,Morning,Afternoon,Evening, None,Disabled}
-        List<Tuple<int, DaySection, bool>> PreferenceOrder = new List<Tuple<int, DaySection, bool>>(new[] {
-            new Tuple<int, DaySection, bool>(0, DaySection.None, false),
-            new Tuple<int, DaySection, bool>(1, DaySection.Morning, false),
-            new Tuple<int, DaySection, bool>(2, DaySection.Afternoon, false),
-            new Tuple<int, DaySection, bool>(3, DaySection.Evening, false),
-            new Tuple<int, DaySection, bool>(4, DaySection.Sleep , false)
-        });
+        protected DateTimeOffset tImeLineStart;
+        protected TimeLine fullDayTImeLine;
+        List<Tuple<int, DaySection, bool, TimeLine>> PreferenceOrder;
+        List<Tuple<int, DaySection, bool, TimeLine>>  DefaultOrder  = new List<Tuple<int, DaySection, bool, TimeLine>>(new[] {
+                new Tuple<int, DaySection, bool, TimeLine>(0, DaySection.None, false, new TimeLine()),
+                new Tuple<int, DaySection, bool, TimeLine>(1, DaySection.Morning, false, new TimeLine()),
+                new Tuple<int, DaySection, bool, TimeLine>(2, DaySection.Afternoon, false, new TimeLine()),
+                new Tuple<int, DaySection, bool, TimeLine>(3, DaySection.Evening, false, new TimeLine()),
+                new Tuple<int, DaySection, bool, TimeLine>(4, DaySection.Sleep , false, new TimeLine())
+            });
+
         //TilerEvent ControlEvent;
-        public TimeOfDayPreferrence()
+        public TimeOfDayPreferrence(TimeLine timeLine)
         {
+            tImeLineStart = timeLine.Start;
+            fullDayTImeLine = timeLine.CreateCopy();
+            PreferenceOrder = new List<Tuple<int, DaySection, bool, TimeLine>>(new[] {
+                new Tuple<int, DaySection, bool, TimeLine>(0, DaySection.None, false, new TimeLine(timeLine.Start, timeLine.Start.AddDays(1).AddTicks(-1))),
+                new Tuple<int, DaySection, bool, TimeLine>(1, DaySection.Morning, false, new TimeLine(timeLine.Start, timeLine.Start.AddHours(6).AddTicks(-1))),
+                new Tuple<int, DaySection, bool, TimeLine>(2, DaySection.Afternoon, false, new TimeLine(timeLine.Start.AddHours(6), timeLine.Start.AddHours(12).AddTicks(-1))),
+                new Tuple<int, DaySection, bool, TimeLine>(3, DaySection.Evening, false, new TimeLine(timeLine.Start.AddHours(12), timeLine.Start.AddHours(18).AddTicks(-1))),
+                new Tuple<int, DaySection, bool, TimeLine>(4, DaySection.Sleep , false, new TimeLine(timeLine.Start.AddHours(18), timeLine.Start.AddHours(24).AddTicks(-1)))
+            });
+            DefaultOrder = PreferenceOrder.ToList();
+        }
+
+        protected void generateTimeFrames(TimeLine timeLine)
+        {
+            tImeLineStart = timeLine.Start;
+            fullDayTImeLine = timeLine.CreateCopy();
+            PreferenceOrder = new List<Tuple<int, DaySection, bool, TimeLine>>(new[] {
+                new Tuple<int, DaySection, bool, TimeLine>(0, DaySection.None, false, new TimeLine(timeLine.Start, timeLine.Start.AddDays(1).AddTicks(-1))),
+                new Tuple<int, DaySection, bool, TimeLine>(1, DaySection.Morning, false, new TimeLine(timeLine.Start, timeLine.Start.AddHours(6).AddTicks(-1))),
+                new Tuple<int, DaySection, bool, TimeLine>(2, DaySection.Afternoon, false, new TimeLine(timeLine.Start.AddHours(6), timeLine.Start.AddHours(12).AddTicks(-1))),
+                new Tuple<int, DaySection, bool, TimeLine>(3, DaySection.Evening, false, new TimeLine(timeLine.Start.AddHours(12), timeLine.Start.AddHours(18).AddTicks(-1))),
+                new Tuple<int, DaySection, bool, TimeLine>(4, DaySection.Sleep , false, new TimeLine(timeLine.Start.AddHours(18), timeLine.Start.AddHours(24).AddTicks(-1)))
+            });
         }
 
         internal void InitializeGrouping(TilerEvent ControlEvent)
         {
+            if (ControlEvent.Rigid)
+            {
+                List<Tuple<int, DaySection, bool, TimeLine>> preferenceOrderCopy = DefaultOrder.ToList();
+                preferenceOrderCopy.RemoveAt(0);
+                List<TimeLine> timeLines = preferenceOrderCopy.Select(timeLine => timeLine.Item4.InterferringTimeLine(ControlEvent.RangeTimeLine)).ToList();
+                PreferenceOrder = new List<Tuple<int, DaySection, bool, TimeLine>>();
+                for (int i=0;i< timeLines.Count; i++)
+                {
+                    TimeLine timeLine = timeLines[i];
+                    if(timeLine != null)
+                    {
+                        PreferenceOrder.Add(new Tuple<int, DaySection, bool, TimeLine>(i, preferenceOrderCopy[i].Item2, preferenceOrderCopy[i].Item3, preferenceOrderCopy[i].Item4.CreateCopy()));
+                    }
+                }
+            }
             return;
         }
         public DaySection getCurrentDayPreference()
@@ -39,9 +81,36 @@ namespace TilerElements
 
         public void setCurrentdayPreference(DaySection preferredSection)
         {
-            Tuple <int, DaySection, bool> preferredOption = PreferenceOrder.SingleOrDefault(preferenee => preferenee.Item2 == preferredSection);
+            Tuple <int, DaySection, bool,TimeLine> preferredOption = PreferenceOrder.SingleOrDefault(preferenee => preferenee.Item2 == preferredSection);
             PreferenceOrder.Remove(preferredOption);
             PreferenceOrder.Insert(0, preferredOption);
+        }
+
+        public void assignSectorBasedOnTIme(DateTimeOffset time, TimeLine daytimeLine)
+        {
+            PreferenceOrder = DefaultOrder.ToList();
+            daytimeLine = daytimeLine ?? fullDayTImeLine;
+            if (daytimeLine.IsDateTimeWithin(time))
+            {
+                if(fullDayTImeLine!= daytimeLine)
+                {
+                    generateTimeFrames(daytimeLine);
+                }
+                Tuple<int, DaySection, bool, TimeLine> daySection = PreferenceOrder.Where(obj => obj.Item2 != DaySection.None).Where(obj =>  obj.Item4.IsDateTimeWithin(time)).First();
+                if (daySection != null)
+                {
+                    setCurrentdayPreference(daySection.Item2);
+                }
+                else
+                {
+                    throw new Exception("Cannot add a day preference not within one of the precalculated day preferences");
+                }
+            }
+            else
+            {
+                throw new Exception("Cannot work with a datetimeoffset that is outside the deay preference for this object");
+            }
+            
         }
 
         public void rejectCurrentPreference()

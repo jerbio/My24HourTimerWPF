1c976f5f5265f379fe7c230176393adfa4ac04db
Jerome Biotidara <Jeromebiotidara@gmail.com>
2017-01-18 06:00:17 +0000

    Initial test to evaluate WhatIf monday instead of tuesdat

---
+++ TilerTests/WhatIf.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TilerFront;
+using My24HourTimerWPF;
+using TilerElements;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TilerTests
+{
+    [TestClass]
+    public class WhatIf
+    {
+        Random random = new Random((int)DateTimeOffset.UtcNow.Ticks);
+        [ClassInitialize]
+        public static void classInitialize(TestContext testContext)
+        {
+            UserAccount currentUser = TestUtility.getTestUser();
+            currentUser.Login().Wait();
+            DateTimeOffset refNow = DateTimeOffset.UtcNow;
+            Schedule schedule = new Schedule(currentUser, refNow);
+            currentUser.DeleteAllCalendarEvents();
+        }
+
+        [TestInitialize]
+        public void cleanUpLog()
+        {
+            UserAccount currentUser = TestUtility.getTestUser();
+            currentUser.Login().Wait();
+            DateTimeOffset refNow = DateTimeOffset.UtcNow;
+            Schedule Schedule = new Schedule(currentUser, refNow);
+            currentUser.DeleteAllCalendarEvents();
+        }
+
+        [ClassCleanup]
+        public static void cleanUpTest()
+        {
+            UserAccount currentUser = TestUtility.getTestUser();
+            currentUser.Login().Wait();
+            DateTimeOffset refNow = DateTimeOffset.UtcNow;
+            Schedule Schedule = new Schedule(currentUser, refNow);
+            currentUser.DeleteAllCalendarEvents();
+        }
+
+        [TestMethod]
+        public void WhatIfMondayInsteadOfTuesday()
+        {
+            List<Location_Elements> locations = TestUtility.getLocations();
+            int mondayLocationIndex = random.Next(locations.Count);
+            Location_Elements mondayLocation = locations[mondayLocationIndex];
+            Location_Elements TuesdayLocation = locations[(mondayLocationIndex + 1)% locations.Count];
+            List<CalendarEvent> mondayEvents = new List<CalendarEvent>();
+            List<CalendarEvent> tuesdayEvents = new List<CalendarEvent>();
+            TimeSpan durationOfCalEvent = TimeSpan.FromHours(1);
+            DateTimeOffset refNow = DateTimeOffset.UtcNow;
+            DateTimeOffset mondayStart = getNextDateForDayOfWeek(DayOfWeek.Monday, refNow);
+            DateTimeOffset tuesdayStart = mondayStart.AddDays(1);
+            int numberOfEvents = 5;
+            for (int i = 0; i < numberOfEvents; i++)
+            {
+                CalendarEvent mondayEvent = TestUtility.generateCalendarEvent(durationOfCalEvent, new Repetition(), mondayStart, mondayStart.AddDays(1),1, false, mondayLocation);
+                CalendarEvent tuesdayEvent = TestUtility.generateCalendarEvent(durationOfCalEvent, new Repetition(), tuesdayStart, tuesdayStart.AddDays(1), 1, false, TuesdayLocation);
+                mondayEvents.Add(mondayEvent);
+                tuesdayEvents.Add(tuesdayEvent);
+            }
+            List<CalendarEvent> allCalendarEvents = mondayEvents.Concat(tuesdayEvents).ToList();
+            UserAccount currentUser = TestUtility.getTestUser();
+            currentUser.Login().Wait();
+            foreach (CalendarEvent calEvent in allCalendarEvents)
+            {
+                Schedule eachSchedule = new Schedule(currentUser, refNow);
+                eachSchedule.AddToScheduleAndCommit(calEvent).Wait();
+            }
+
+            DateTimeOffset wednesdayStart = getNextDateForDayOfWeek(DayOfWeek.Wednesday, refNow);
+            CalendarEvent wednesdayEvent = TestUtility.generateCalendarEvent(durationOfCalEvent, new Repetition(), mondayStart, wednesdayStart.AddDays(1), 1, false, TuesdayLocation);
+            Schedule schedule = new Schedule(currentUser, refNow);
+            schedule.AddToScheduleAndCommit(wednesdayEvent).Wait();
+
+            //schedule.WhatIf()
+        }
+
+        public DateTimeOffset getNextDateForDayOfWeek(DayOfWeek dayOfeek, DateTimeOffset referenceTime)
+        {
+            DateTimeOffset retValue;
+
+            if(referenceTime.DayOfWeek != dayOfeek)
+            {
+                int dayCount = ((int)referenceTime.DayOfWeek + 7);
+                int dayDiff = dayCount - (int)dayOfeek;
+                retValue = referenceTime.AddDays(dayDiff);
+            }
+            else
+            {
+                retValue = referenceTime;
+                retValue = retValue.LocalDateTime;
+            }
+
+            return retValue;
+
+        }
+    }
+}

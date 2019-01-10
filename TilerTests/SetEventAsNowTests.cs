6ca86a33507121b36ec755b03201cca850cd91dc
Jerome Biotidara <jeromebiotidara@gmail.com>
2019-01-09 20:54:02 -0700

    Still yet to implement setasnow, I already created class but need to make sure tests run

--- TilerTests/SetEventAsNowTests.cs
+++ TilerTests/SetEventAsNowTests.cs
@@ -35,26 +35,9 @@ namespace TilerTests
             Schedule.persistToDB().Wait();
             testEvent = TestUtility.getCalendarEventById(testEvent.getId, user);
             Assert.IsTrue(testEvent.ActiveSubEvents.OrderBy(subevent => subevent.Start).First().Start == refNow);
-
-            Schedule = new TestSchedule(user, refNow);
-            TimeSpan procrastinationSpan = TimeSpan.FromHours(5);
-            var procrastinateResult = Schedule.ProcrastinateJustAnEvent(testEvent.AllSubEvents.OrderBy(subEvent => subEvent.Start).First().getId, procrastinationSpan);
-            Assert.IsNull(procrastinateResult.Item1);
-            Schedule.persistToDB().Wait();
-            Schedule = new TestSchedule(user, refNow);
-
-            CalendarEvent testEventCopy = TestUtility.getCalendarEventById(testEvent.getId, user);
-            DateTimeOffset latestTime = refNow.Add(procrastinationSpan);
-            Assert.IsTrue(testEventCopy.ActiveSubEvents.OrderBy(subEvent => subEvent.Start).First().Start >= latestTime);
-            Assert.IsTrue(testEventCopy.getProcrastinationInfo.PreferredStartTime >= latestTime);
         }
         [TestMethod]
         public void setCalendarEventAsNowPastDeadline()
-        {
-        }
-
-        [TestMethod]
-        public void setSubCalendarEventAsNow()
         {
             TestSchedule Schedule;
             TilerUser tilerUser = TestUtility.createUser();
@@ -66,43 +49,22 @@ namespace TilerTests
             TimeSpan duration = TimeSpan.FromHours(2);
             DateTimeOffset start = refNow;
             DateTimeOffset end = refNow.AddHours(7);
+            CalendarEvent testEvent = TestUtility.generateCalendarEvent(tilerUser, duration, new Repetition(), start, end, 2, false);
             Schedule = new TestSchedule(user, refNow);
-            CalendarEvent testEvent = TestUtility.generateCalendarEvent(tilerUser,duration, new Repetition(), start, end, 2, false);
             Schedule.AddToScheduleAndCommit(testEvent).Wait();
-
-            user = TestUtility.getTestUser(userId: tilerUser.Id);
-            user.Login().Wait();
-            TimeSpan duration0 = TimeSpan.FromHours(2);
-            DateTimeOffset start0 = refNow;
-            DateTimeOffset end0 = refNow.AddHours(7);
-            Schedule = new TestSchedule(user, refNow);
-            CalendarEvent testEvent0 = TestUtility.generateCalendarEvent(tilerUser,duration0, new Repetition(), start0, end0, 2, false);
-            Schedule.AddToScheduleAndCommit(testEvent0).Wait();
-
-            user = TestUtility.getTestUser(userId: tilerUser.Id);
-            user.Login().Wait();
-            TimeSpan duration1 = TimeSpan.FromHours(2);
-            DateTimeOffset start1 = refNow;
-            DateTimeOffset end1 = refNow.AddHours(7);
-            Schedule = new TestSchedule(user, refNow);
-            CalendarEvent testEvent1 = TestUtility.generateCalendarEvent(tilerUser,duration1, new Repetition(), start1, end1, 2, false);
-            Schedule.AddToScheduleAndCommit(testEvent1).Wait();
-
-            user = TestUtility.getTestUser(userId: tilerUser.Id);
-            user.Login().Wait();
-            Schedule = new TestSchedule(user, refNow);
+            DateTimeOffset newRefNow = end.AddDays(1);
+            Schedule = new TestSchedule(user, newRefNow);
             var setAsNowResult = Schedule.SetCalendarEventAsNow(testEvent.getId);
             Schedule.persistToDB().Wait();
+            testEvent = TestUtility.getCalendarEventById(testEvent.getId, user);
+            Assert.IsTrue(testEvent.ActiveSubEvents.OrderBy(subevent => subevent.Start).First().Start == newRefNow);
 
-            Schedule = new TestSchedule(user, refNow);
-            TimeSpan procrastinationSpan = TimeSpan.FromHours(5);
-            var procrassinateResult = Schedule.ProcrastinateJustAnEvent(testEvent.AllSubEvents.OrderBy(subEvent => subEvent.Start).First().getId, procrastinationSpan);
-            Assert.IsNull(procrassinateResult.Item1);
-            Schedule.persistToDB().Wait();
-            Schedule = new TestSchedule(user, refNow);
-            CalendarEvent testEventCopy = TestUtility.getCalendarEventById(testEvent.getId, user);
-            DateTimeOffset latestTime = refNow.Add(procrastinationSpan);
-            Assert.IsTrue(testEventCopy.ActiveSubEvents.OrderBy(subEvent => subEvent.Start).First().Start >= latestTime);
+        }
+
+        [TestMethod]
+        public void setSubCalendarEventAsNow()
+        {
+            
         }
         [TestMethod]
         public void setSubCalendarEventAsNowPastDeadline()

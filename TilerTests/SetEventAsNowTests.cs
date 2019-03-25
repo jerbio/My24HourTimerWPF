810cb1072810e76b7d6251fc6336b098f5e92576
Jerome Biotidara <jeromebiotidara@gmail.com>
2019-03-24 21:18:40 -0600

    Added tests

--- TilerTests/SetEventAsNowTests.cs
+++ TilerTests/SetEventAsNowTests.cs
@@ -167,5 +167,119 @@ namespace TilerTests
             CalendarEvent afterSetAsNowCalendarEvent = TestUtility.getCalendarEventById(testEvent.getId, user);
             Assert.IsTrue(afterSetAsNowCalendarEvent.isTestEquivalent(beforeSetAsNowCalendarEvent));
         }
+        [TestMethod]
+        public void CalendarEventRestrictedAsNow()
+        {
+            TestSchedule Schedule;
+            TilerUser tilerUser = TestUtility.createUser();
+            UserAccount user = TestUtility.getTestUser(userId: tilerUser.Id);
+            tilerUser = user.getTilerUser();
+            user.Login().Wait();
+            DateTimeOffset refNow = TestUtility.parseAsUTC("12:00AM 12/2/2017");
+            Schedule = new TestSchedule(user, refNow);
+            TimeSpan duration = TimeSpan.FromHours(4);
+            DateTimeOffset start = refNow;
+            DateTimeOffset end = refNow.Add(duration);
+            TimeLine repetitionRange = new TimeLine(start, start.AddDays(13).AddHours(-23));
+            DayOfWeek startingWeekDay = start.DayOfWeek;
+            Repetition repetition = new Repetition();
+            TimeSpan fullDuration = duration + duration;
+            var restrictionProfile = new RestrictionProfile(start, fullDuration);
+            CalendarEventRestricted testEvent = TestUtility.generateCalendarEvent(tilerUser, duration, repetition, start, end, 1, false, restrictionProfile: restrictionProfile, now: Schedule.Now) as CalendarEventRestricted;
+            Schedule.AddToScheduleAndCommit(testEvent).Wait();
+            DateTimeOffset newRefNow = end.AddHours(1);
+            Schedule = new TestSchedule(user, newRefNow);
+            var setAsNowResult = Schedule.SetCalendarEventAsNow(testEvent.getId);
+            Schedule.persistToDB().Wait();
+            CalendarEvent afterSetAsNowCalendarEvent = TestUtility.getCalendarEventById(testEvent.getId, user);
+            SubCalendarEvent nowSubEvent = afterSetAsNowCalendarEvent.AllSubEvents.SingleOrDefault(subEvent => subEvent.Start == newRefNow);
+            Assert.IsNotNull(nowSubEvent);
+        }
+
+        [TestMethod]
+        public void SubCalendarEventRestrictedAsNow()
+        {
+            TestSchedule Schedule;
+            TilerUser tilerUser = TestUtility.createUser();
+            UserAccount user = TestUtility.getTestUser(userId: tilerUser.Id);
+            tilerUser = user.getTilerUser();
+            user.Login().Wait();
+            DateTimeOffset refNow = TestUtility.parseAsUTC("12:00AM 12/2/2017");
+            Schedule = new TestSchedule(user, refNow);
+            TimeSpan duration = TimeSpan.FromHours(4);
+            DateTimeOffset start = refNow;
+            DateTimeOffset end = refNow.Add(duration);
+            TimeLine repetitionRange = new TimeLine(start, start.AddDays(13).AddHours(-23));
+            DayOfWeek startingWeekDay = start.DayOfWeek;
+            Repetition repetition = new Repetition();
+            var restrictionProfile = new RestrictionProfile(start, duration + duration);
+            CalendarEventRestricted testEvent = TestUtility.generateCalendarEvent(tilerUser, duration, repetition, start, end, 1, false, restrictionProfile: restrictionProfile, now: Schedule.Now) as CalendarEventRestricted;
+            Schedule.AddToScheduleAndCommit(testEvent).Wait();
+            DateTimeOffset newRefNow = start.AddHours(1);
+            Schedule = new TestSchedule(user, newRefNow);
+            SubCalendarEvent subEvent = testEvent.ActiveSubEvents.First();
+            var setAsNowResult = Schedule.SetEventAsNow(subEvent.getId);
+            Schedule.persistToDB().Wait();
+            SubCalendarEvent subCalendarEventNow = TestUtility.getSubEVentById(subEvent.getId, user);
+            Assert.IsTrue(subCalendarEventNow.Start == newRefNow);
+        }
+
+        [TestMethod]
+        public void CalendarEventRestrictedAsNowOutsideRestrictedFrame()
+        {
+            TestSchedule Schedule;
+            TilerUser tilerUser = TestUtility.createUser();
+            UserAccount user = TestUtility.getTestUser(userId: tilerUser.Id);
+            tilerUser = user.getTilerUser();
+            user.Login().Wait();
+            DateTimeOffset refNow = TestUtility.parseAsUTC("12:00AM 12/2/2017");
+            Schedule = new TestSchedule(user, refNow);
+            TimeSpan duration = TimeSpan.FromHours(4);
+            DateTimeOffset start = refNow;
+            TimeLine repetitionRange = new TimeLine(start, start.AddDays(13).AddHours(-23));
+            DateTimeOffset end = repetitionRange.End.AddDays(14);
+            DayOfWeek startingWeekDay = start.DayOfWeek;
+            Repetition repetition = new Repetition();
+            TimeSpan fullDuration = duration + duration;
+            var restrictionProfile = new RestrictionProfile(start.AddHours(2), fullDuration);
+            CalendarEventRestricted testEvent = TestUtility.generateCalendarEvent(tilerUser, duration, repetition, start, end, 1, false, restrictionProfile: restrictionProfile, now: Schedule.Now) as CalendarEventRestricted;
+            Schedule.AddToScheduleAndCommit(testEvent).Wait();
+            DateTimeOffset newRefNow = start.AddHours(1);
+            Schedule = new TestSchedule(user, newRefNow);
+            var setAsNowResult = Schedule.SetCalendarEventAsNow(testEvent.getId);
+            Schedule.persistToDB().Wait();
+            CalendarEvent afterSetAsNowCalendarEvent = TestUtility.getCalendarEventById(testEvent.getId, user);
+            SubCalendarEvent nowSubEvent = afterSetAsNowCalendarEvent.AllSubEvents.SingleOrDefault(subEvent => subEvent.Start == newRefNow);
+            Assert.IsNotNull(nowSubEvent);
+        }
+
+        [TestMethod]
+        public void SubsCalendarEventRestrictedAsNowOutsideRestrictedFrame()
+        {
+            TestSchedule Schedule;
+            TilerUser tilerUser = TestUtility.createUser();
+            UserAccount user = TestUtility.getTestUser(userId: tilerUser.Id);
+            tilerUser = user.getTilerUser();
+            user.Login().Wait();
+            DateTimeOffset refNow = TestUtility.parseAsUTC("12:00AM 12/2/2017");
+            Schedule = new TestSchedule(user, refNow);
+            TimeSpan duration = TimeSpan.FromHours(4);
+            DateTimeOffset start = refNow;
+            TimeLine repetitionRange = new TimeLine(start, start.AddDays(13).AddHours(-23));
+            DateTimeOffset end = repetitionRange.End.AddDays(14);
+            DayOfWeek startingWeekDay = start.DayOfWeek;
+            Repetition repetition = new Repetition();
+            TimeSpan fullDuration = duration + duration;
+            var restrictionProfile = new RestrictionProfile(start.AddHours(2), fullDuration);
+            CalendarEventRestricted testEvent = TestUtility.generateCalendarEvent(tilerUser, duration, repetition, start, end, 1, false, restrictionProfile: restrictionProfile, now: Schedule.Now) as CalendarEventRestricted;
+            Schedule.AddToScheduleAndCommit(testEvent).Wait();
+            DateTimeOffset newRefNow = start.AddHours(1);
+            Schedule = new TestSchedule(user, newRefNow);
+            SubCalendarEvent subEvent = testEvent.ActiveSubEvents.First();
+            var setAsNowResult = Schedule.SetEventAsNow(subEvent.getId, true);
+            Schedule.persistToDB().Wait();
+            SubCalendarEvent subCalendarEventNow = TestUtility.getSubEVentById(subEvent.getId, user);
+            Assert.IsTrue(subCalendarEventNow.Start == newRefNow);
+        }
     }
 }

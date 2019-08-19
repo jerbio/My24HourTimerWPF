f2230731b13ef110112fc61467ee6f8cb58bbeb6
Jerome Biotidara <jeromebiotidara@gmail.com>
2019-08-19 07:28:10 +0000

    Improved tiler performance by modifying repeating events

--- TilerTests/PerformanceTest.cs
+++ TilerTests/PerformanceTest.cs
@@ -72,12 +72,38 @@ namespace TilerTests
             DayOfWeek startingWeekDay = start.DayOfWeek;
             TimeLine repetitionTimeLine = new TimeLine(start, end);
             TimeLine eachRepeatFrame = new TimeLine(start, start.AddDays(1).AddMilliseconds(-1));
+            TimeLine rangeOfLookup = new TimeLine(start.AddDays(-30), start.AddDays(60));
+            watch.Stop();
             List<CalendarEvent> allCalendarEvents = new List<CalendarEvent>();
 
             for (int i = 0; i < numberOfEvents; i++)
             {
                 // cannot simply run addroscheduleAndCommit because you'll un into memory issues
 
+                //getAllEnabledSubCalendarEvent(TimelineForData, now, true, DataRetrivalOption.Ui).
+                LogControl LogAccess = user.ScheduleLogControl;
+                watch.Restart();
+
+                var task = LogAccess.getAllEnabledSubCalendarEvent(rangeOfLookup, Schedule.Now, retrievalOption: DataRetrivalOption.Ui);
+                task.Wait();
+                var allSubs = task.Result.ToList();
+
+                
+                watch.Stop();
+                TimeSpan finshedSubEventRetrieval = watch.Elapsed;
+                
+                watch.Reset();
+                watch.Start();
+                var taskCal = LogAccess.getAllEnabledCalendarEvent(rangeOfLookup, Schedule.Now, retrievalOption: DataRetrivalOption.Ui);
+                taskCal.Wait();
+                var allCals = taskCal.Result.ToList();
+                watch.Stop();
+                TimeSpan finshedCalEventRetrieval = watch.Elapsed;
+
+
+                Debug.WriteLine("Sub event retrieval took " + finshedSubEventRetrieval.ToString());
+                Debug.WriteLine("Cal event retrieval took " + finshedCalEventRetrieval.ToString());
+                Debug.WriteLine("-------------------------------------------------------");
                 TestUtility.reloadTilerUser(ref user, ref tilerUser);
                 Schedule = new TestSchedule(user, refNow);
                 Repetition repetition = new Repetition(repetitionTimeLine, Repetition.Frequency.DAILY, eachRepeatFrame);
